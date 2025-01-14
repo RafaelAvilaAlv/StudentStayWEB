d7f9377dbed9a6edfda6df8bec5da65b3cbc14e3
RafaelAvila <milton.avila.est@tecazuay.edu.ec>
2025-01-14 09:44:37 -0500

    Inicializando el proyecto StudentStayPCWEB

---
+++ src/main/java/com/proyecto/api/StuadentStayPcwebApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuadentStayPcwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuadentStayPcwebApplication.class, args);
+	}
+
+}
